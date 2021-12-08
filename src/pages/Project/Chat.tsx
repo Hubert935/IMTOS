79deffd6c889f5a9108e87595a5cd382fc2a60e3
max <max.h.lundqvist@gmail.com>
2021-12-08 11:28:12 +0100

    Worked on the chat function

--- src/pages/Project/Chat.tsx
+++ src/pages/Project/Chat.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState, useEffect } from "react";
 import styles from "./Chat.module.css"
 
+
 const Chat = () => {
     interface ToppersData {
         key: number;
@@ -38,28 +39,39 @@ const Chat = () => {
 
         if(pos==="right"){
             return (
-                <div style={{textAlign: "right"}}>
-                    <div>
-                    <p>{message}</p>
-                    </div>
+                <>
+                <br/>
+                <div style={{textAlign: "right",  width:"100%"}}>
+                    <p style={{marginRight:"17px", fontSize:"12px"}}>Anna</p>
+                    <div style={{background:"green", width:"90%",  borderRadius:"10px", marginLeft:"42px"}}>
+                    <p style={{marginRight:"10px"}}>{message}</p>
+                    </div> 
                 </div>
+                </>
             )
         }else{
             return (
-                <div style={{textAlign: "left"}}>
-                    <div>
-                    <p>{message}</p>
+                <>
+                <br />
+                <div style={{textAlign: "left", width:"100%"}}>
+                     <p style={{marginLeft:"14px", fontSize:"12px"}}>Pelle</p>
+                    <div style={{ background:"grey", width:"90%",  borderRadius:"10px", marginLeft:"10px"}}>
+                        <p style={{marginLeft:"10px"}}>{message}</p>
                     </div>
                 </div>
+                </>
             )
         }
     
     }
 
     return (
-        <div style={{width:"100%", textAlign:"center"}}>
-        <div style={{width:"75%"}}>
-            <div >
+        <div style={{width:"100%", textAlign:"center", height:"25vh"}}>
+        <div style={{width:"75%", marginLeft:"12%"}}>
+            <div style={{width:"100%", 
+            height:"500px", overflow:"auto", background:"beige",
+             borderRadius:"20px", 
+                        }} >
                 
                     {chatMessage.map((chatMessage) => (
                         create(chatMessage.msg, chatMessage.position)
@@ -67,6 +79,10 @@ const Chat = () => {
                     ))}
                 
             </div>
+            
+
+        </div>
+        <div style={{position:"absolute", marginTop:"10px", marginLeft:"13%"}}>
             <form onSubmit={submitForm}>
                 <input
                     value={term}
@@ -75,10 +91,16 @@ const Chat = () => {
                     placeholder="Type your message..."
                     className="input"
                 />
-                <button type="submit" className="btn">Submit</button>
+                <button type="submit" className="btn"
+                style={{
+                    background:"blue", borderRadius:"10px", width:"80px", border:"none", 
+                    height:"23px", boxShadow:"1px 1px 5px 2px rgba(0,0,0,0.59)"
+                }}
+                >
+                    Submit
+                </button>
             </form>
-
-        </div>
+            </div>
         </div>
     )
 }
