0d554e790abbc5a58009583408d832ad87040818
fridaaringskog <74775549+fridaaringskog@users.noreply.github.com>
2021-12-08 12:16:06 +0100

    deleted some from startscreen

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,14 @@ import {
   Switch,
 } from "react-router-dom";
 import styles from "./App.module.css";
-import { Example } from "./pages/Example/Example";
-import { Home } from "./pages/Home/Home";
 import { Project } from "./pages/Project/Project"
 import { Divider } from "./ui-lib/Divider/Divider";
 import { Header } from "./ui-lib/Header/Header";
 import { DataProvider } from "./shared/DataProvider";
 
-const title = "React Demo 2021";
+const title = "Office Dashboard";
 
 const routes = [
-  { label: "🏠 Home", path: "/home", component: Home },
-  { label: "🧪 Example", path: "/example", component: Example },
   { label: "Project", path:"/project", component: Project}
 ];
 
@@ -27,11 +23,12 @@ function App() {
       <Router>
         <div className={styles.container}>
           <div className={styles.content}>
+            
             <Header title={title} routes={routes} />
             <Divider />
             <div className={styles.pageContent}>
               <Routes />
-            </div>
+            </div> 
           </div>
         </div>
       </Router>
