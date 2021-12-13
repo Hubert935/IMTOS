6f34f6189d1d39a438f9ebea4ae930a95dd18b27
Hubert935 <max.h.lundqvist@gmail.com>
2021-12-13 09:13:44 +0100

    Added sign in animation for team as well as team signed in at the time, changed joke a little bit

--- src/pages/Project/Joke/Joke.tsx
+++ src/pages/Project/Joke/Joke.tsx
@@ -4,28 +4,49 @@ import { Spacer } from "../../../ui-lib/Spacer/Spacer";
 
 export const Joke =()=>{
     const [joke , setJoke] = useState(" ");
+    const [response, setRespone] = useState({})
 
-    function generateJOD() {
+    function generateJOD(typeOfJoke) {
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
             // Access the result here
-            var jokeOfTheDay = this.response.toString().split("text")[1].split(":")[1].split("}}],")[0];
-            console.log(jokeOfTheDay)
-            setJoke(jokeOfTheDay);      
+            // var jokeOfTheDay = this.response.toString().split("text")[1].split(":")[1].split("}}],")[0];
+            // console.log(jokeOfTheDay)
+            //setJoke(jokeOfTheDay);  
+            setRespone(this.response)   
+            const start = this.response.toString().search("text")
+            const end = this.response.toString().search("copyright")
+            // let length = this.response.toString().substr(end +9, 2);
+            // console.log(this.response.toString())
+            // console.log(start) 
+            // console.log(length)
+            let finalJoke = this.response.toString().substr(start+7, -(start - (end - 13)));
+            finalJoke = finalJoke.replace(/\\/g, '');
+            finalJoke = finalJoke.replace(/rn/g, '');
+            console.log(finalJoke)
+            setJoke(finalJoke);
+           
         }
         };
-        xhttp.open("GET", "https://api.jokes.one/jod?category=animal", true);
+        xhttp.open("GET", `https://api.jokes.one/jod?category=${typeOfJoke}`, true);
         xhttp.setRequestHeader("Content-type", "application/json");
         xhttp.setRequestHeader("X-JokesOne-Api-Secret", "YOUR API HERE");
         xhttp.send();
         var array = xhttp.response
-        console.log(array)
+        console.log(array)  
         console.log(xhttp)
     }
 
     return(<>
-    <Button label={"Hit me!"} onClick={()=>{generateJOD()}}></Button>
+    <div style={{display:"flex"}}>
+    {/* <Button label={"1"} onClick={()=>{generateJOD("blonde")}}></Button> */}
+    <Spacer size={16} />
+    <Button label={"Animal"} onClick={()=>{generateJOD("animal")}}></Button>
+    <Spacer size={16} />
+    <Button label={"random"} onClick={()=>{generateJOD("jod")}}></Button>
+    <Spacer size={16} />
+    </div>
     <Spacer size={16} />
     <p>{joke}</p>
     </>)
--- src/pages/Project/Project.tsx
+++ src/pages/Project/Project.tsx
@@ -9,10 +9,13 @@ import { Joke } from "./Joke/Joke";
 import { SpacerDivider } from "../../ui-lib/Divider/SpacerDivider";
 import _ from "lodash";
 import { Spacer } from "../../ui-lib/Spacer/Spacer";
+import { useState } from "react";
+import Help from "../Help/Help";
 
 export const Project = () => {
     const teamState = useTeamState();
     const teamEventMean = useTeamEventMean();
+    const [atWork, setAtWork] = useState(false);
     const happyValue = _.round(
         teamEventMean?.find((m) => m.type === "emotion-1")?.value ?? 0,
         2
@@ -22,12 +25,39 @@ export const Project = () => {
         2
     );
 
+    const setWorking = (working) => {
+        if(working === false){
+            setAtWork(false)
+        }else{
+            setAtWork(true)
+        }
+        console.log(working)
+    }
+
+    const help =()=>{
+        const val = teamState?.state.find((s) => s.key === "motion")?.value ?? "Unknown"
+        console.log('heehehehhehehe' + val)
+        if(val === "0" || "unknown"){
+            return (
+                <>
+                    <p>😖</p>
+                </>
+            );
+        } else{
+            return (
+                <>
+                    <p>😎</p>
+                </>
+            );
+        }
+    }
+
     return (
         <div className={styles.gridContainer}>
             <div className={styles.leftCol}>
-                <Workday/>
+                <Workday onBuy={setWorking}/>
                 <SpacerDivider/>
-                <Workers />
+                <Workers working={atWork}/>
             </div>
             <Chat/>
             <div className={styles.rightCol}>
@@ -35,7 +65,10 @@ export const Project = () => {
                 <SpacerDivider/>
                 <h3>Your desk:</h3>
                 <Spacer size={16}/>
-                <p>{teamState?.state.find((s) => s.key === "motion")?.value ?? "Unknown"}</p>
+                
+                <Help motion={teamState?.state.find((s) => s.key === "motion")?.value}/>
+                {/* <p>{teamState?.state.find((s) => s.key === "motion")?.value ?? "Unknown"}</p> */}
+                {/* {help()} */}
                 <SpacerDivider/>
                 <h3>Office Mood:</h3>
                 <Spacer size={16}/>
